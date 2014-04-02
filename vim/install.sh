aa0d0c6c3e09c97e753c315e11c2032075b3776a
Jiangzhou He <hjz@google.com>
2014-04-02 15:54:48 -0700

    Let vim run PluginInstall for vundle automatically.

--- vim/install.sh
+++ vim/install.sh
@@ -3,7 +3,8 @@ source $(dirname $0)/../install-lib.sh
 
 if ! [ -d ~/.vim/bundle/vundle ]; then
   echo "Vundle is missing, try to install it..."
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && \
+      vim +PluginInstall +qall
 fi
 
 safe_symlink $LOCAL_DIR/.vimrc ~/.vimrc
